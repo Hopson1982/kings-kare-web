651d653c6537fa9b1c6bae9055104b774b175442
Brion Hopson <hopsonb@c02f47ekmd6m.wwt.com>
2021-06-07 15:56:49 -0500

    intial

--- component/Layout.js
+++ component/Layout.js
@@ -7,7 +7,7 @@ const Layout = ({ children }) => {
     return (
         <>
             <Meta />
-            <Nav />
+            {/* <Nav /> */}
             <div className={styles.container} >
                 <Header />
                 <main>
