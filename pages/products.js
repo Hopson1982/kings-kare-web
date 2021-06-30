a5d3c81cc3eac1f941fc5688aca90e6273f7984f
Brion Hopson <hopsonb@c02f47ekmd6m.wwt.com>
2021-06-30 14:18:55 -0500

    products page

--- pages/products.js
+++ pages/products.js
@@ -9,8 +9,8 @@ export default function ProductsPage({ listProducts }) {
 }
 
 export const getStaticProps = async () => {
-    const response = await fetch('http://localhost:3000/api');
-//    const response = await fetch('https://kings-kare-web-16i2k3o2r-hopson1982.vercel.app//api');
+  // const response = await fetch('http://localhost:3000/api');
+   const response = await fetch('https://kings-kare-web-16i2k3o2r-hopson1982.vercel.app//api');
 
     
 
